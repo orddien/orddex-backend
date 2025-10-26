5cfb5f4071a1aecd520729818d7321163fafc914
orddien <orddien@gmail.com>
2025-10-25 23:40:06 -0300

    Update db.js

--- backend/src/db.js
+++ backend/src/db.js
@@ -1,5 +1,16 @@
 import pkg from 'pg';
 const { Pool } = pkg;
-const pool = new Pool({ connectionString: process.env.DATABASE_URL });
-export const query=(t,p)=>pool.query(t,p);
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false, // 🔥 evita erro de certificado self-signed
+  },
+});
+
+export const query = (text, params) => pool.query(text, params);
+
 export default pool;
